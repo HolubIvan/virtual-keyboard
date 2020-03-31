90522a59c74afd653885bd67562f21b697f01852
Ivan Holub <ivanholub.js@gmail.com>
2020-03-31 14:58:19 +0300

    feat: fix past problems, add active class on keypress, change listenClick and listenPress functions

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-const keys = [
-  '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
-  'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '|',
-  'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"', 'Enter',
-  'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲',
-  'Ctr', 'Opt', 'Cmd', 'Space', 'Cmd', 'Opt', '◄', '▼', '►',
-];
+const eventCode = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ControlLeft', 'AltLeft', 'MetaLeft', 'Space', 'MetaRight', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
 
-const keysBrake = ['Backspace', '|', 'Enter', '▲'];
+const eventKeyEng = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '|', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Ctr', 'Alt', 'Meta', ' ', 'Meta', 'Alt', '◄', '▼', '►'];
 
+const eventKeyRus = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+  'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'ё', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/', '▲', 'Ctr', 'Alt', 'Meta', ' ', 'Meta', 'Alt', '◄', '▼', '►'];
 
-function addAllKeys(arr) {
-  const keysArray = [];
+// final key array
+const keyOnInput = [];
+
+const textarea = document.createElement('textarea');
+
+// add keys from array to html markup
+
+function addAllKeys(arr, arr1) {
+  const result = [];
   for (let i = 0; i < arr.length; i++) {
-    const br = document.createElement('br');
     const button = document.createElement('button');
     button.classList.add('keyboard__key');
-    if (arr[i] === 'Space') {
+    button.setAttribute('data', arr[i]);
+    if (button.getAttribute('data') === 'Space') {
       button.classList.add('keyboard__key-wide');
-    } else if (arr[i] === 'Shift' || arr[i] === 'Enter' || arr[i] === 'Backspace' || arr[i] === 'Tab' || arr[i] === '▲') {
+    } else if (button.getAttribute('data') === 'ShiftLeft' || button.getAttribute('data') === 'Enter' || button.getAttribute('data') === 'Backspace' || button.getAttribute('data') === 'Tab' || arr[i] === 'ArrowUp') {
       button.classList.add('keyboard__key-middle');
-    } else if (arr[i] === 'CapsLock') {
+    } else if (button.getAttribute('data') === 'CapsLock') {
       button.classList.add('keyboard__key-short');
     }
-    button.textContent = arr[i];
-    keysArray.push(button);
-    if (keysBrake.indexOf(arr[i]) !== -1) {
-      keysArray.push(br);
-    }
+    button.textContent = arr1[i];
+    result.push(button);
   }
-  return keysArray;
+  return result;
 }
 
+// init function: create keyboard, add keys from html markup key and add to document
+
 function init() {
-  const textarea = document.createElement('textarea');
   textarea.classList.add('textarea');
 
   const keyboard = document.createElement('div');
@@ -42,13 +43,69 @@ function init() {
   keyboardKeys.classList.add('keyboard__keys');
 
   keyboard.append(keyboardKeys);
-  keyboardKeys.append(...addAllKeys(keys));
+  keyboardKeys.append(...addAllKeys(eventCode, eventKeyEng));
 
   document.body.append(textarea);
   document.body.append(keyboard);
 }
 
+// listen for key press function and add key to textarea
+
+function listenKeyPress() {
+  document.addEventListener('keypress', (event) => {
+    keyOnInput.push(event.key);
+    textarea.value += keyOnInput[keyOnInput.length - 1];
+  });
+}
+
+// listen for click on keyboard and add key to textarea
+
+function listenClick() {
+  document.addEventListener('click', (event) => {
+
+    // listen for delete
+    if (event.target.textContent === 'Backspace') {
+      keyOnInput.pop();
+      textarea.value = keyOnInput.join('');
+    } else if (event.target.textContent === 'Ctr' || event.target.textContent === 'Alt' || event.target.textContent === 'Meta' || event.target.textContent === 'Tab' || event.target.textContent === 'Shift' || event.target.textContent === 'Enter' || event.target.textContent === 'CapsLock') {
+      return false;
+    } else {
+      keyOnInput.push(event.target.textContent);
+      textarea.value += keyOnInput[keyOnInput.length - 1];
+    }
+  });
+
+  // document.querySelector('[data="CapsLock"]').addEventListener('click', (event) => {
+
+  // });
+}
+
+
+// add active class on key down
+function activeClassKeyDown() {
+  document.addEventListener('keydown', (event) => {
+    document.querySelectorAll('button').forEach((e) => {
+      e.classList.remove('active');
+    });
+    document.querySelector(`.keyboard__key[data="${event.code}"]`).classList.add('active');
+  });
+}
+
+// remove active class on key up
+
+function activeClassKeyUp() {
+  document.addEventListener('keyup', () => {
+    document.querySelectorAll('button').forEach((e) => {
+      e.classList.remove('active');
+    });
+  });
+}
+
 
 window.addEventListener('load', () => {
   init();
+  listenKeyPress();
+  listenClick();
+  activeClassKeyDown();
+  activeClassKeyUp();
 });
